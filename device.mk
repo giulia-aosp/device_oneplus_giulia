abd1b927c20917146aadb23fcf7b5eb3cf05b780
Bruno Martins <bgcngm@gmail.com>
2025-02-07 00:02:13 +0000

    giulia: Remove old vibrator effects
    Change-Id: I7388500fb127287904a48dfb200a04ae813b0d58
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.oplus
 
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USE_EFFECT_STREAM,true)
-$(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator/include)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
