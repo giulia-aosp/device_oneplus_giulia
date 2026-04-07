2445ttv/android_device_oneplus_giulia

giulia: Import QTI vibrator effect and rename
Change-Id: I60e4623e5d3fc5c5582e70c7f751c0928f77563d

device.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.giulia)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \

giulia-aosp/device_oneplus_giulia

device.mk
@@ -100,6 +100,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 $(call soong_config_set_bool,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus.giuliac)
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \